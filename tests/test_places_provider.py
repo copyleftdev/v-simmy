76b1acdcc9853854147c008b17cb58e2097023a8
Don Johnson <dj@codetestcode.io>
2018-04-11 21:26:42 -0700

    create test stub for place providers

---
+++ tests/test_places_provider.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import unittest
+import pickle as pkl
+from libs.Places import PlacesProvider
+
+
+class TestsPlacesProvider(unittest.TestCase):
+
+    def setUp(self):
+        self.place_provider = PlacesProvider()
+
+    def test_get_all_countries_return_countries(self):
+        pass
+
+    def test_get_all_states_return_states(self):
+        pass
+
+    def test_get_all_places_return_places(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
