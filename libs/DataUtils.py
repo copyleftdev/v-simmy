b0151e637e02da8e4fa6344d164c8745ad974229
Don Johnson <dj@codetestcode.io>
2018-04-14 18:43:21 -0700

    change header info

--- libs/DataUtils.py
+++ libs/DataUtils.py
@@ -1,13 +1,14 @@
 
 import requests
 
+
 class RestRouter(object):
 
-    def __init__(self, host,route):
+    def __init__(self, host, route):
         self.host = host
         self.route = route
 
     def send_post(self, payload):
-        header = {"Content-Type":"application/json"}
+        header = {"Content-Type": "application/json"}
         url = "http://{}{}".format(self.host, self.route)
         req = requests.post(url, data=payload, headers=header)
